d637b5ae650659132235c0596d4a29d8c61563fb
Rob Lee <rob.lee@live.co.uk>
2021-12-13 08:06:41 +0000

    First commit
    Draft of  script
    Started the readme
    Added .ignore files
    ZSH exec script (not sure why this is here...)

---
+++ README.md
@@ -0,0 +1,15 @@
+`Reli.sh` -- sauce for your shell
+=================================
+
+Relish makes anything better. I'm aiming to do the same to you day to day shell
+experience with a script wrapper to let you fully customise and embellish your 
+most used tools and commands.
+
+Installation
+------------
+
+### Brew
+
+`brew install reli.sh`
+
+
---
+++ reli.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env sh
+
+# Reli.sh -- it's sauce for your shell
+
+
+
+## reli.sh is a shell script wrapper that lets you customise your tools and shell experience.
+
+usage=$(cat <<EOU
+
+    reli.sh -- shell sauce
+
+
+
+EOU
+)
+
+## Prerequisites
+
+### Installation
+
+install_dir=/var/opt/reli.sh
+init=$install_dir/.init.d
+
+if [ ! -d $install_dir ]
+then
+  echo $usage
+fi
+
+
+### Environment
+
+#### Proxy
+
+proxy_prts="http,https"
+proxy_addr="protoproxy.local"
+proxy_port="80808"
+proxy_user="tom.bom"
+proxy_pass='cretins'
+
+
+## Script Wrapper
+
+case $1 in
+  init)
+    if [ -e $init ]
+    then
+      echo "already initialised"
+    else
+      echo "relishing environment"
+      sudo mkdir $init
+    fi
+    ;;
+esac
+
+echo "EDITOR IS $EDITOR"
+echo "SHELL  IS $SHELL"
+
+
+
+sleep 600
+
+
+
